134bd7d30644360c6a7056cec534a8920cc78b0d
mando100 <armandosoto100@gmail.com>
2014-08-22 17:03:44 -0300

    Consulta y Base de datos

---
+++ Consultas.sql
@@ -0,0 +1,13 @@
+﻿Consulta 1:
+
+SELECT * 
+FROM Contacto
+WHERE  day(Cumpleaños) = day(now()) && month(Cumpleaños) = month(now()) 
+
+
+Consulta 2:
+
+SELECT Telefono,Email,Nombre_Contacto
+FROM Contacto
+WHERE  day(Cumpleaños) = day(now()) && month(Cumpleaños) = month(now()) 
+
