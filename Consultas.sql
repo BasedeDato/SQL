2424f02d243b813d7e8340180dd73be4243de454
armando <armandosoto100@gmail.com>
2014-08-25 17:47:25 -0300

    Consulta 3 y 4 agregadas

--- Consultas.sql
+++ Consultas.sql
@@ -11,3 +11,19 @@ SELECT Telefono,Email,Nombre
 FROM Contacto, Cliente
 WHERE  day(Cumpleaños) = day(now()) && month(Cumpleaños) = month(now()) 
 
+Consulta 3: 
+
+Select Nombre,Razon_Social
+From Ciudades, Cliente
+Where Cliente.ID_Ciudad = 224
+and Ciudades.ID_Ciudad = 224
+Order by Nombre
+
+
+Consulta 4: 
+
+Select *
+From Ciudades, Provincia
+Where Provincia = 'Cordoba'
+And Ciudades.ID_Provincia = Provincia.ID_Provincia
+Order by Codigo_Postal
