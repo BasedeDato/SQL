eb5c30e8d258066ba4f11f8006f157371f703fb4
armando <armandosoto100@gmail.com>
2014-08-25 17:14:30 -0300

    Base de dato creda con valores random, en total hay 500 clientes y 500 contactos, 23 provincias y 500 ciudades

--- Ciudades.py
+++ Ciudades.py
@@ -5,7 +5,7 @@ def randomword():
    return ''.join(random.choice(string.lowercase) for i in 
 range(int(random.choice(string.digits))+1))
 
-f = open('InsertDB', 'w')
+f = open('CiudadesDB', 'w')
 
 
 #INSERT INTO `Ciudades` (`ID_Ciudad`, `Nombre_Ciudad`, `ID_Provincia`, 
@@ -18,10 +18,10 @@ f.write("`Ciudades`(`ID_Ciudad`,`Nombre_Ciudad`,`ID_Provincia`,`Codigo_Postal`)"
 )
 f.write("\nVALUES\n")
 
-for i in range (100):
+for i in range (499):
     x = str(random.randint(1,23))
     y = str(random.randint(1000,9999))
-    f.write("('"+str(i)+"','"+randomword()+"','"+x+"','"+y+"'),\n")
+    f.write("('"+str(i+1)+"','"+randomword()+"','"+x+"','"+y+"'),\n")
 
 print f
 
